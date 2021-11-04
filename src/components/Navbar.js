eec8e353686e450efa4d8907af5b898372d151ed
Emad Aldomari <ea@ea.sa>
2021-11-04 15:52:02 +0300

    useNavigate example

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+    const navigate = useNavigate();
     return <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Anime</a>
+      <Link class="navbar-brand" to="/">Anime</Link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <Link to="/" class="nav-link active" aria-current="page" href="#">Home</Link>
+            <a onClick={() => navigate("/")} class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
         </ul>
 
